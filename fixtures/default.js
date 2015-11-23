964fe5d565c61b33b4fed00e7094c5ddcecf8dfe
Alexander Zaretskii <al.zaretskii@gmail.com>
2015-11-23 15:06:10 +0200

    add DEXTER module

--- fixtures/default.js
+++ fixtures/default.js
@@ -63,7 +63,9 @@ module.exports = _.merge({
              */
             input: {
                 //Replace VAR1 with the name of an expected input, and add more inputs as needed.
-                VAR1: 'foo'
+                id: 'id_message',
+                userId: 'gmail_address@gmail.com'
+
             }
         }
     }
--- index.js
+++ index.js
@@ -1,4 +1,24 @@
+var _ = require('lodash'),
+  google = require('googleapis'),
+  OAuth2 = google.auth.OAuth2;
+
+var requireParams = ['id', 'userId'];
+
 module.exports = {
+    checkAuthOptions: function (step, dexter) {
+        _.map(requireParams, function (reqParam) {
+            if(!step.input(reqParam).first()) {
+
+                this.fail('A ' + reqParam +' input variable is required for this module');
+            }
+        }, this);
+
+        if(!dexter.environment('access_token')) {
+
+            this.fail('A access_code environment variable is required for this module');
+        }
+    },
+
     /**
      * The main entry point for the Dexter module
      *
@@ -6,8 +26,17 @@ module.exports = {
      * @param {AppData} dexter Container for all data used in this workflow.
      */
     run: function(step, dexter) {
-        var results = { foo: 'bar' };
-        //Call this.complete with the module's output.  If there's an error, call this.fail(message) instead.
-        this.complete(results);
+
+        this.checkAuthOptions(step, dexter);
+
+        var oauth2Client = new OAuth2();
+        oauth2Client.setCredentials({access_token: dexter.environment('access_token'), refresh_token: dexter.environment('refresh_token')});
+
+        google.options({ auth: oauth2Client });
+        google.gmail('v1').users.messages.delete({auth: oauth2Client, id: step.input('id', null).first(), userId: step.input('userId', null).first()}, function (err) {
+
+            err? this.fail(err) : this.complete({isRemoved: true});
+        }.bind(this));
+
     }
 };
