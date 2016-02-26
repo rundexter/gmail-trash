1a233d4fba5e221a10dc4a5a7cbab5ea15341ebc
Mike Friedman <friedo@friedo.com>
2016-02-26 11:30:28 -0500

    parallelizing for multiple msgs and consistency with other gmail modules

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var _ = require('lodash'),
+    Q = require( 'q' ),
     util = require('./util.js'),
     google = require('googleapis'),
     service = google.gmail('v1');
@@ -7,6 +8,16 @@ var pickInputs = {
         'id': { key: 'id', validate: { req: true } },
         'userId': { key: 'userId', validate: { req: true } }
     };
+
+var trash_msg = function( app, service, user, msg_id ) {
+    var deferred = Q.defer();
+    service.users.messages.delete( { 'id': msg_id, 'userId': user, function( err ) {
+        if ( err ) { return deferred.reject( err ) }
+        else       { return deferred.resolve() }
+    } )
+
+    return deferred.promise;
+};
 
 module.exports = {
     /**
@@ -19,21 +30,25 @@ module.exports = {
         var OAuth2 = google.auth.OAuth2,
             oauth2Client = new OAuth2(),
             credentials = dexter.provider('google').credentials();
-        var inputs = util.pickInputs(step, pickInputs),
-            validateErrors = util.checkValidateErrors(inputs, pickInputs);
-
-        if (validateErrors)
-            return this.fail(validateErrors);
 
         // set credential
         oauth2Client.setCredentials({
             access_token: _.get(credentials, 'access_token')
         });
         google.options({ auth: oauth2Client });
-        service.users.messages.delete(inputs, function (err) {
 
-            err? this.fail(err) : this.complete({ success: true });
-        }.bind(this));
+        var ids  = step.input( 'id' );
+        var user = step.input( 'userId' ).first();
 
+        var deletes = [ ];
+        var app = this;
+
+        ids.forEach( function( id ) {
+            deletes.push( trash_msg( app, service, user, id ) );
+        } )
+
+        Q.all( removes )
+          .then( function() { app.complete() } )
+          .fail( function( err ) { app.fail( err ) } );
     }
 };
